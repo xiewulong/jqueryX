61600b1286179337a50731e0ddfcdf80b53ed50d
xiewulong <xiewulong@vip.qq.com>
2013-11-14 09:41:07 -0500

    jqueryX

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,27 @@ module.exports = function(grunt){
 			options: {
 				banner: '/*!\n * <%=pkg.description%> v<%=pkg.version%>\n * author: <%=pkg.author%>\n * create: 2013/5/16\n * update: <%=grunt.template.today("yyyy/mm/dd")%>\n */\n\n '
 			},
-			jqueryX: {
+			compress: {
 				files: {
 					'jquery.x.min.js': ['jquery.x.js']
 				}
 			}
-		}
+		},
+    jsdoc: {
+      gerenate: {
+        src: ['jquery.xjs'],
+        options: {
+          destination: 'doc'
+        }
+      }
+    }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jsdoc-plugin');
 
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', [
+    'uglify',
+    //'jsdoc' //dependent on java
+  ]);
 }
