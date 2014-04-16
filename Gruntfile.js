b8b4c04f52abc5698a6cede284f9d24956a7c8c8
xiewulong <xiewulong@vip.qq.com>
2014-04-16 13:39:34 +0800

    jqueryX

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,20 @@
 module.exports = function(grunt){
 	'use strict';
 
-	var path_yuidoc = '.';
+	var path	= '.',
+		src		= '.',
+		dest	= '.';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		clean: [path_yuidoc + '/doc'],
+		clean: [path + '/doc'],
 		uglify: {
 			options: {
 				banner: '/*!\n * <%= pkg.description %> v<%= pkg.version %>\n * <%= pkg.author.name %> <<%= pkg.author.email %>>\n * <%= pkg.author.url %>\n * <%= pkg.license.url %>\n * create: 2013/5/16\n * update: <%= grunt.template.today("yyyy/mm/dd") %>\n */\n\n'
 			},
 			compress: {
 				files: {
-					'jquery.x.min.js': ['jquery.x.js']
+					dest + '/jquery.x.min.js': [src + '/jquery.x.js']
 				}
 			}
 		},
@@ -29,34 +31,52 @@ module.exports = function(grunt){
 					attributeEmit: 'true',
 					selleck: 'true',
 					//ignorePaths: [],
-					paths: [path_yuidoc],
-					outdir: path_yuidoc + '/doc'
+					paths: [path],
+					outdir: path + '/doc'
 				}
 			}
-		}
+		},
 		/*
 		jsdoc: {
 			generate: {
-				src: [path_yuidoc + '/jquery.x.js'],
+				src: [path + '/jquery.x.js'],
 				options: {
 					template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
 					configure: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json',
-					destination: path_yuidoc + '/doc'
+					destination: path + '/doc'
 				}
 			}
 		}
 		*/
+		watch: {
+			scripts: {
+				files: ['jquery.x.js'],
+				tasks: [
+					'clean',
+					'uglify',
+					'yuidoc'
+				]
+			},
+			configFiles: {
+				files: ['Gruntfile.js'],
+				options: {
+					reload: true
+				}
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-yuidoc');
 	//grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
 		'clean',
 		'uglify',
-		'yuidoc'
+		'yuidoc',
 		//'jsdoc' //dependent on java
+		'watch'
 	]);
-}
+};
